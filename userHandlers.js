e79d217a016849c87af9778c768dc26a68a2ca9a
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-05-08 15:26:36 +0200

    added exercice express 03

--- userHandlers.js
+++ userHandlers.js
@@ -1,30 +1,52 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
-    database
+  database
     .query("select * from users")
-    .then((result) => {
-        const users = result[0];
-        res.status(200).json(users);
+    .then(([users]) => {
+      res.json(users);
     })
     .catch((err) => {
-        console.error(err);
-        res.status(500).send("ERROR 500");
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
     });
 };
 
 const getUserById = (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    database
+  database
     .query("select * from users where id = ?", [id])
-    .then (([users]) => {
-        if (users[0] != null) {
-            res.status(200).json(users[0]);
-        } else {
-            res.status(404).send("Not Found");
-        }
-});
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
-module.exports = {getUsers, getUserById};
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+    .query("INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+    [firstname, lastname, email, city, language])
+    .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving the user");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  postUser,
+};
